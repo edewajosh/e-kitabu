cef7b853df19e1ce7d7803f14d18b1eea46e1e05
edewajosh <joshedewa@gmail.com>
2018-07-18 14:46:02 +0300

    Massive changes

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse, request
+from .models import Book, Author, BookInstance, Genre
 
 def index(request):
-    return HttpResponse("Hello Local Libarary App")
+    
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authours = Author.objects.count()
+
+    context = {
+        'num_books' : num_books,
+        'num_instance' : num_instances,
+        'num_instances_available' : num_instances_available,
+        'num_authours' : num_authours,
+    }
+    
+    return render(request, 'index.html',context)
